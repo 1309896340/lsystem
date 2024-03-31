7a92d43e7bee8e5e10ab2c40d838f533b38466a7
windwhisper <1309896340@qq.com>
2024-03-31 23:00:47 +0800

    ready for implementing D0L-System

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+
+typedef std::map<std::string, std::string> LRule;
+
+
+class LSystem{
+    protected:
+        std::string axiom;
+        LRule rule;
+    public:
+        LSystem(const std::string& axiom, const LRule& rule){
+            this->axiom = axiom;
+            this->rule = rule;
+        }
+        std::string generate(int iteration){
+            std::string tmp(this->axiom);
+            for(int i=0;i<iteration;i++)
+                tmp = this->iterate(tmp);
+            return tmp;
+        }
+        virtual std::string iterate(const std::string &p) = 0; //apply "rule" of "p" for one itration
+};
+
+class D0LSystem : public LSystem{
+    public:
+        D0LSystem(const std::string& axiom, const LRule& rule):LSystem(axiom, rule){}
+
+        virtual std::string iterate(const std::string& p){
+            // implementation for D0L-System
+        }
+};
 
 int main(int argc, char **argv){
     int ac = 1;
